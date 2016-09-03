8772f08e0afc53c12054fb7126175b6f5fd818ba
Xavier Claude <contact@xavierclaude.be>
2016-09-03 16:44:25 +0200

    use https for upload

--- attach.php
+++ attach.php
@@ -20,7 +20,7 @@
   $uri = $_SERVER['REQUEST_URI'];
   $folder = substr($uri, 0, strrpos($uri, "/"));
 
-  $fileurl = "http://$sname/$folder/$new_file";
+  $fileurl = "https://$sname/$folder/$new_file";
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
